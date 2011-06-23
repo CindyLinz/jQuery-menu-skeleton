bb24908259d2aca4f81ef00e9ea7c70c3a937a2c
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2011-06-23 16:42:46 +0800

    first release

---
+++ jquery-menu-skeleton.js
@@ -0,0 +1,49 @@
+/*!
+ * jQuery Menu Skeleton Library v0.01
+ * https://github.com/CindyLinz/jQuery-menu-skeleton
+ *
+ * Copyright 2011, Cindy Wang (CindyLinz)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Date: 2011.6.23
+ */
+(function($){
+    var origin_menuSkeleton = $.fn.menuSkeleton;
+
+    var active_list = [];
+    var menuSkeleton = function(child_field, offset){
+	this.each(function(){
+	    $(this).mouseenter(function(){
+		while(true){
+		    var last = active_list.pop();
+		    if( last===undefined )
+			break;
+		    if( $(last).has(this).length ){
+			active_list.push(last);
+			break;
+		    }
+		    $(last).hide();
+		}
+
+		var child = $(this).data(child_field);
+		if( child!==undefined ){
+		    active_list.push(child);
+		    var this_offset = $(this).offset();
+		    if( offset!==undefined ){
+			this_offset.top += offset.top;
+			this_offset.left += offset.left;
+		    }
+		    //$('#dump').text( active_list.join(',') + '[' + this_offset.left + ',' + this_offset.top + ']' );
+		    $(child).show().offset(this_offset);
+		}
+	    });
+	});
+
+	return this;
+    };
+    menuSkeleton.noConflict = function(){
+	$.fn.menuSkeleton = origin_menuSkeleton;
+	return menuSkeleton;
+    };
+    $.fn.menuSkeleton = menuSkeleton;
+})(jQuery)
